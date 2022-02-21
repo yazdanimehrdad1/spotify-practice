7631fc608e5c20bc69846ece17d933a01bcb35ca
yazdanimehrdad1 <yazdani.mehrdad1@gmail.com>
2022-02-21 11:16:40 -0800

    handled jwt expiration

--- pages/playlist/[id].tsx
+++ pages/playlist/[id].tsx
@@ -35,11 +35,23 @@ const Playlist = ({playlist})=>{
 }
 
 export const getServerSideProps = async ({query,req})=>{
-    const {id} = validateToken(req.cookies.MY_ACCESS_TOKEN)
+    let user
+    try {
+        user = validateToken(req.cookies.MY_ACCESS_TOKEN)
+    } catch (error) {
+        return{
+            redirect:{
+                permanent:false,    
+                destination: '/signin'
+            }
+        }
+    }
+
+    // const {id} = validateToken(req.cookies.MY_ACCESS_TOKEN)
     const [playlist] = await prisma.playlist.findMany({
         where:{
             id: +query.id,
-            userId: id
+            userId: user.id
         },
         include:{
             songs:{
